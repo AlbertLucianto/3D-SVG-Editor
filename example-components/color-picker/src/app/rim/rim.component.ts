72a8ead93f2a30d19337acbdf23b805efb9241fb
Albert Lucianto <albertlucianto@gmail.com>
2017-11-09 02:08:39 +0800

    integrate with component and html template

--- example-components/color-picker/src/app/rim/rim.component.ts
+++ example-components/color-picker/src/app/rim/rim.component.ts
@@ -1,9 +1,14 @@
-import { WithSubStore, select, dispatch } from '@angular-redux/store';
+import { WithSubStore, select, select$, dispatch } from '@angular-redux/store';
 import { Component, Input } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import { rimComponentReducer } from './rim.reducers';
 import { SelectAttributeAction } from './rim.actions';
+import { ColorType, IFill } from './rim.model';
+
+export const getColor = (fill$: Observable<IFill>): Observable<ColorType> =>
+  fill$.map(fill => fill.color.toString());
 
 @WithSubStore({
   basePathMethodName: 'getBasePath',
@@ -15,7 +20,9 @@ import { SelectAttributeAction } from './rim.actions';
   styleUrls: ['./rim.component.scss'],
 })
 export class RimComponent {
-  @select('activeAttribute') readonly selectedAttribute$: Observable<string>;
+  @select('activeAttribute')      readonly selectedAttribute$: Observable<string>;
+  @select$('fill', getColor)      readonly fillColor$: Observable<string>;
+  @select$('outline', getColor)   readonly outlineColor$: Observable<string>;
   getBasePath = () => ['rim'];
   @dispatch() selectAttribute = (attribute: string) => {
     return new SelectAttributeAction(attribute);
