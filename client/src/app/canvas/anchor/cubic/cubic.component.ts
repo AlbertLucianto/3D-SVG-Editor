a56dc9ce49a419bd273b05e2eea5cb1d0c0f92c7
Albert Lucianto <albertlucianto@gmail.com>
2017-11-26 02:18:13 +0800

    cubic anchor component

---
+++ client/src/app/canvas/anchor/cubic/cubic.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+	selector: 'app-cubic',
+	templateUrl: './cubic.component.html',
+	styleUrls: ['./cubic.component.scss'],
+	encapsulation: ViewEncapsulation.None,
+})
+export class CubicAnchorComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
